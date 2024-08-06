23ed106bf12fa55286bf8887cb59eb437a617bbf
iishu <ishiksrivastava27594@gmail.com>
2024-08-06 23:56:01 +0530

    Variable Def

---
+++ index.js
@@ -0,0 +1,11 @@
+const accountId=14453; //change nahi kiya ja sakta
+let accountEmail="ishikasrivastava@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+// accountId=2
+accountEmail="sar@google.com"
+accountPassword="34324"
+accountCity="Delhi"
+console.log([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
