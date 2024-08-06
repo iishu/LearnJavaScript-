ba663b5a594bb52522fef356f0c48bcafdb96175
iishu <ishiksrivastava27594@gmail.com>
2024-08-07 00:25:42 +0530

    datatypes

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";   //treat all js code as newer version
+
+// alert("Please")
+
+let name="ishika" //string
+let age=23; //number
+let isActive=false; //boolean
+let state; //undefined
+
+  //number=>2 to power 53
+  //string=>""
+  //boolean=>true/false
+  //bigInt
+  //null=>standalone value
+  //underfined
+  //symbol=>unique identifier
+
+  console.log(typeof "Ishika");
