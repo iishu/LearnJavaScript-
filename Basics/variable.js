ec387699b48b014ff658d3fddeeb0a5663660e79
iishu <ishiksrivastava27594@gmail.com>
2024-08-07 00:07:57 +0530

    commit basics folder

--- Basics/variable.js
+++ Basics/variable.js
@@ -2,7 +2,10 @@ const accountId=14453; //change nahi kiya ja sakta
 let accountEmail="ishikasrivastava@google.com"
 var accountPassword="12345"
 accountCity="Jaipur"
-
+/*
+prefer not to use var 
+because of block scope and functional scope
+*/
 // accountId=2
 accountEmail="sar@google.com"
 accountPassword="34324"
