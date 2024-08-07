70c1041cf107fdda7f534c33a367a75b8647214a
iishu <ishiksrivastava27594@gmail.com>
2024-08-07 19:32:30 +0530

    commit type conversion

---
+++ Basics/03_typeConversion.js
@@ -0,0 +1,20 @@
+// let score='33';
+// console.log(typeof score);
+// console.log(typeof (score));
+//mujhe ye string ko number k form mein store krna hai to hum ab=>
+// let valueInNumber =Number(score);
+// console.log(typeof valueInNumber);
+
+// let value=true;
+// console.log(typeof value); //boolean
+// let valueInNumber =Number(value);
+//console.log(valueInNumber); // 1
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn =Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
