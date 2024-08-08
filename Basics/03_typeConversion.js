4ba5b8697e7608680e011954a2520287756bb22d
iishu <ishiksrivastava27594@gmail.com>
2024-08-08 20:02:20 +0530

    Comparison operator

--- Basics/03_typeConversion.js
+++ Basics/03_typeConversion.js
@@ -10,11 +10,35 @@
 // let valueInNumber =Number(value);
 //console.log(valueInNumber); // 1
 
-let isLoggedIn=1
+// let isLoggedIn=1
 
-let booleanIsLoggedIn =Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn =Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //"33"=>33
 //"33abc"=>NaN
 //true=>1; false=>0
+
+//1=>true
+//0=>false
+//""=>false
+//"abc"=>true
+
+                        //  Operations
+
+// let str1="hello"
+// let str2=" Ishu"
+// let str3=str1 +  str2
+// console.log(str3);
+
+// console.log("1"+2);  //12
+// console.log(1+"2");  //12
+// console.log("1"+2+2);  //122
+// console.log(1+2+"2");  //32
+
+// console.log(+true); //1
+// console.log(+"");  //0
+
+// let num1, num2, num3
+
+// num1=num2=num3=2+2 
